062d4e693f8c9322cdf528d5863a0bd9a7be68d8
lesoura <marvirtlester.mb@gmail.com>
2025-12-10 14:58:26 +0800

    Hero BG Test 7

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -50,13 +50,13 @@ export default function Hero() {
       id="hero"
       className="relative w-full min-h-screen flex flex-col items-start justify-center px-4 sm:px-10 md:px-24 text-left overflow-hidden"
     >
-      <div
-        className="absolute inset-0 bg-fixed sm:object-[center_top_25%] bg-center bg-cover"
-        style={{
-          backgroundImage: "url('/images/me.jpg')",
-          opacity: 0.6,
-        }}
-      />
+      <div className="absolute inset-0 overflow-hidden">
+  <img
+    src="/images/me.jpg"
+    className="w-full h-full object-cover sm:object-[center_top_25%] opacity-60"
+    alt="Background"
+  />
+</div>
 
 
       <div className="absolute inset-0 bg-black/40" />
